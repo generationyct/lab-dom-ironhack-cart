2ad255e08d4aa4e5d5482f3288aef42b8d42ca9f
Devgen <dev@generationyct.nl>
2020-01-30 13:00:59 +0100

    Finished iteration 1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,26 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $unitPrice = document.getElementById('#pu-number');
+var $quantity = document.getElementById('quantity').value
+var $subTotal = +document.querySelector('#subtot-span').textContent
 
-function updateSubtot($product) {
+// function updateSubtot($product) {
+
   // Iteration 1.1
+const unitPrice = document.querySelectorAll('#pu-number')
+  for (let i = 0; i < unitPrice.length; i++) {
+    $unitPrice = +unitPrice[i].innerText
+  }
+
+const updateQuantity = document.getElementById('quantity').addEventListener("click", () => {
+  $quantity = document.getElementById('quantity').value
+  calcSubTotal()
+})
+
+const calcSubTotal = () => {
+  let subTotal = $unitPrice * $quantity
+  $subTotal = subTotal
+  document.querySelector('#subtot-span').innerHTML = $subTotal
 }
 
 function calcAll() {
